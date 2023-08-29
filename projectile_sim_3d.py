43d54c2f153d7f1554e9a1f155ab59cda69fb387
Lloyd Bush <lloydtbush@gmail.com>
2023-08-29 17:40:15 +0200

    added 3d capability

--- projectile_sim_3d.py
+++ projectile_sim_3d.py
@@ -1,12 +1,13 @@
 import math
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 import csv
 
 def truncate(n, decimals=3):
     multiplier = 10 ** decimals
     return int(n * multiplier) / multiplier
 
-def ideal():
+""" def ideal():
     v0=20
     g=-9.81
     alpha=45
@@ -21,7 +22,7 @@ def ideal():
         idealX=v0*t
         idealY=g/2*t*t+v0*t*math.sin(math.radians(alpha))
         listParabolX.append(idealX)
-        listParabolY.append(idealY)
+        listParabolY.append(idealY) """
 
 #constant:
 g=-9.81
@@ -32,56 +33,64 @@ ar=0.078
 dt=0.01
 
 #initial:
-v0=20.0
+v0x=20.0
+v0y=5.0
+v0z=10.0
 alpha=45.0
 x0=0.0
 y0=0.0
+z0=0.0
 
 #variable:
 t=0.0
 
 x=x0
 y=y0
-
-v0x=math.cos(math.radians(alpha))*v0
-v0y=math.sin(math.radians(alpha))*v0
+z=z0
 
 vx=v0x
 vy=v0y
+vz=v0z
 
 ax=0.0
 ay=0.0
+az=0.0
 
 fx=0.0
 fy=0.0
+fz=0.0
 
 fg=0.0
 
 fdx=0.0
 fdy=0.0
+fdz=0.0
 
 maxX=0.0
 maxY=0.0
+maxZ=0.0
 
 startX=x
 startY=y
+startZ=z
 
 #lists
 listT=[]
 listX=[]
 listY=[]
+listZ=[]
 listVx=[]
 listVy=[]
+listVz=[]
 listAx=[]
 listAy=[]
-listParabolX=[]
-listParabolY=[]
+listAz=[]
 
 #listParabolX=[] #ideal flight curve x-values
 #listParabolY=[] #ideal flight curve y-values
 
 print("\nSTARTING...\n")
-print("t,x,y,vx,vy,ax,ay")
+print("t,x,y,z,vx,vy,vz,ax,ay,az")
 
 # open csv file in the write mode
 f=open("./data/data.csv","w",encoding="UTF8")
@@ -90,31 +99,35 @@ f=open("./data/data.csv","w",encoding="UTF8")
 writer=csv.writer(f)
 
 #header for csv file
-header="t","x","y","vx","vy","ax","ay"
+header="t","x","y","z","vx","vy","vz","ax","ay","az"
 
 # write a row to the csv file
 writer.writerow(header)
 
 #sim loop
-while y >= 0.0:
-    print(truncate(t,2),truncate(x,3),truncate(y,3),truncate(vx,3),truncate(vy,3),truncate(ax,3),truncate(ay,3))
+while z >= 0.0:
+    print(truncate(t,2),truncate(x,3),truncate(y,3),truncate(z,3),truncate(vx,3),truncate(vy,3),truncate(vz,3),truncate(ax,3),truncate(ay,3),truncate(az,3))
 
-    dataRow=truncate(t,2),truncate(x,3),truncate(y,3),truncate(vx,3),truncate(vy,3),truncate(ax,3),truncate(ay,3)
+    dataRow=truncate(t,2),truncate(x,3),truncate(y,3),truncate(z,3),truncate(vx,3),truncate(vy,3),truncate(vz,3),truncate(ax,3),truncate(ay,3),truncate(az,3)
     # write a row to the csv file
     writer.writerow(dataRow)
     
     listT.append(t)
     listX.append(x)
     listY.append(y)
+    listZ.append(z)
     listVx.append(vx)
     listVy.append(vy)
+    listVz.append(vz)
     listAx.append(ax)
     listAy.append(ay)
+    listAz.append(az)
     
     t+=dt
 
     v0x=vx
     v0y=vy
+    v0z=vz
 
 
     x0=v0x*dt
@@ -130,6 +143,13 @@ while y >= 0.0:
         maxY=y+y0
 
     y+=y0
+
+    z0=v0z*dt
+
+    if z<(z+z0):
+        maxZ=z+z0
+
+    z+=z0
     
     fg=m*g
     
@@ -142,24 +162,39 @@ while y >= 0.0:
         fdy=-0.5*rho*cd*vy*vy*ar
     else:
         fdy=0.5*rho*cd*vy*vy*ar
+    
+    if vz>0.0:   
+        fdz=-0.5*rho*cd*vy*vy*ar
+    else:
+        fdz=0.5*rho*cd*vy*vy*ar
 
     fx=fdx
-    fy=fg+fdy
+    fy=fdy
+    fz=fg+fdz
 
     ax=fx/m
     ay=fy/m
+    az=fz/m
 
     v0x=ax*dt
     v0y=ay*dt
+    v0z=az*dt
 
     vx+=v0x
     vy+=v0y
+    vz+=v0z
 
 # close csv file
 f.close()
 
 print("maxX:",truncate(maxX,3))
 print("maxY:",truncate(maxY,3))
+print("maxZ:",truncate(maxZ,3))
+
+print(listX) #debug
+print(listY) #debug
+print(listZ) #debug
+
 print("\nFINISHED")
 
 #ax=plt.axes()
@@ -172,13 +207,14 @@ fig = plt.figure()
 """fig.patch.set_facecolor("gray")
 fig.patch.set_alpha(0.6)"""
 
-ideal()
-plt.axes().set_aspect('equal')
-plt.plot(listX,listY,color='b',label="sim",linestyle='-')
-plt.plot(listParabolX,listParabolY,color='r',label="ideal",linestyle='--')
-plt.title("Projectile Flight Path")
-plt.xlabel("x / m")
-plt.ylabel("y / m")
-plt.legend()
+#ideal()
 
+ax = plt.axes(projection="3d")
+ax.set_aspect('equal')
+ax.plot3D(listX,listY,listZ,color='b',label="sim",linestyle='-')
+#plt.plot(listParabolX,listParabolY,color='r',label="ideal",linestyle='--')
+plt.title("Projectile Flight Path")
+#plt.xlabel("x / m")
+#plt.ylabel("y / m")
+plt.legend()
 plt.show()
